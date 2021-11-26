8bcd8722f70bf4ad54753b0df81b7446b1799a54
Lovis1234 <90246498+Lovis1234@users.noreply.github.com>
2021-11-26 10:05:21 +0100

    Verwijder

--- pagina1.php
+++ pagina1.php
@@ -1,12 +1,31 @@
 <?php
-session_start();
-$_SESSION['naam'] = "Alex";
-//$leeftijd = 42;
-//$cart = array();            // tijdelijke variabele op de huidige pagina
-//$cart[$stockItemID] = 1;
-//$cart[105932] = 7;	     // puur als voorbeeld: 2e artikel in de lijst
-//$_SESSION['cart'] = $cart;  // sessievariabele neemt de huidige cart op
-$naam = $_SESSION['naam'];
-print("<p>Hallo " . $naam . "</p>");
-?>
-<a href="pagina2.php">Ga naar pagina 2</a>
+//session_start();
+//$_SESSION['naam'] = "Alex";
+////$leeftijd = 42;
+////$cart = array();            // tijdelijke variabele op de huidige pagina
+////$cart[$stockItemID] = 1;
+////$cart[105932] = 7;	     // puur als voorbeeld: 2e artikel in de lijst
+////$_SESSION['cart'] = $cart;  // sessievariabele neemt de huidige cart op
+//$naam = $_SESSION['naam'];
+//print("<p>Hallo " . $naam . "</p>");
+//?>
+<!--<a href="pagina2.php">Ga naar pagina 2</a>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Including Bootstrap Icons in HTML</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <!-- Bootstrap Font Icon CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+</head>
+<body>
+<h1><i class="bi-globe"></i> Hello, world!</h1>
+
+<!-- Bootstrap JS Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- verwijder.php
+++ verwijder.php
@@ -1,10 +1,25 @@
 <?php
-include "CartFuncties.php";
-$cart = getCart();                          // eerst de huidige cart ophalen
-
-if(array_key_exists(1, $cart)){  //controleren of $stockItemID(=key!) al in array staat
-    unset($cart[1]);                   //zo ja:  aantal met 1 verhogen
+include "cartfuncties.php";
+include __DIR__ . "/header.php";
+//$cart = array(1 => 1);
+$cart = getCart();
+$id = $_GET['idprod'];
+//print_r($cart); ?>
+<h1>Je staat op het punt om een product uit de winkelwagen te verwijderen.</h1>
+<form action="Cart.php">
+        <input type="submit" name="annuleren" value="Annuleren">
+</form>
+<form>
+        <input type="submit" name="doorgaan" value="Doorgaan">
+    <input type="hidden" name="idprod" value="<?php print($id); ?>">
+    </form>
+<?php
+if(isset($_GET["doorgaan"])) {//controleren of $stockItemID(=key!) al in array staat
+    unset($cart[$id]);                   //zo ja:  aantal met 1 verhogen
+    saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
+    print_r($cart);
+    header("Location:Cart.php");
 }
-
-saveCart($cart);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
-print_r($cart);
+if(isset($_GET["annuleren"])) {//controleren of $stockItemID(=key!) al in array staat
+    header("Location:Cart.php");
+}
