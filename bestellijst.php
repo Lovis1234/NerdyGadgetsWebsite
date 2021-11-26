7aee366d1af3bcc8eb5f4c9c4df9531fdf506bc6
Lovis1234 <90246498+Lovis1234@users.noreply.github.com>
2021-11-26 10:25:10 +0100

    Bestel

--- bestellijst.php
+++ bestellijst.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Winkelwagen</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+</head>
+<body>
+<h1>Bestel lijst</h1>
+<form>
