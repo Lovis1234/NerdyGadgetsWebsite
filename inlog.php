f3a9491e15d6aaf1ab8a0a917a351bfb2d90f594
Lovis1234 <90246498+Lovis1234@users.noreply.github.com>
2021-12-07 14:19:08 +0100

    update

--- inlog.php
+++ inlog.php
@@ -1,81 +1,60 @@
 <?php
-// Initialize the session
-//session_start();
 include __DIR__ . "/header.php";
-// Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: index.php");
-    exit;
 }
-
-
-// Define variables and initialize with empty values
 $email = $password = "";
 $email_err = $password_err = $login_err = "";
 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // Check if email is empty
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty(trim($_POST["email"]))){
         $email_err = "Please enter email.";
     } else{
         $email = trim($_POST["email"]);
     }
 
-    // Check if password is empty
+    // Kijken of email leeg is
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
 
-    // Validate credentials
+    // Wanneer geen error
     if(empty($email_err) && empty($password_err)){
-        // Prepare a select statement
+        // Inloggegevens ophalen
         $sql = "SELECT id, email, password FROM login WHERE email = ?";
 
         if($stmt = mysqli_prepare($databaseConnection, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_email);
-
-            // Set parameters
             $param_email = $email;
 
-            // Attempt to execute the prepared statement
+            // Uitvoering
             if(mysqli_stmt_execute($stmt)){
-                // Store result
+                // Opslaan
                 mysqli_stmt_store_result($stmt);
-
-                // Check if email exists, if yes then verify password
+                // Kijken naar email dan naar wachtwoord
                 if(mysqli_stmt_num_rows($stmt) == 1){
-                    // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-
-                            // Store data in session variables
+                            // Hier is hij goedgekeurt
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["email"] = $email;
-
-                            // Redirect user to welcome page
                             header("location: index.php");
                         } else{
-                            // Password is not valid, display a generic error message
                             $login_err = "Invalid email or password.";
                         }
                     }
                 } else{
-                    // email doesn't exist, display a generic error message
+                    // email bestaat niet
                     $login_err = "Invalid email or password.";
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
-
-            // Close statement
             mysqli_stmt_close($stmt);
         }
     }
