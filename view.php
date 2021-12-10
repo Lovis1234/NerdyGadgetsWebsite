6b2231aacec4e58916e470d309ed3112d7bb53ce
JonathanEverts <94914995+JonathanEverts@users.noreply.github.com>
2021-12-10 09:36:21 +0100

    Review

--- view.php
+++ view.php
@@ -97,39 +97,10 @@ $button="";
             <form method="post">
                 <input type="submit" name="button" value="Voeg toe aan winkelmand" class="button" id="button">
             </form>
-        <form method="post">
-            <input type="submit" name="button2" value="Plaats een review voor dit product!" class="button" id="ReviewButton">
-        </form>
         </ul>
         <div id="StockItemDescription">
             <h3>Artikel beschrijving</h3>
             <p><?php print $StockItem['SearchDetails']; ?></p>
-
-            <?php
-$idarray = getReviewIDUit($databaseConnection,$_GET['id']);
-
-        if (getReviewCount($databaseConnection,$_GET['id']) >= 1) {
-            print("<h1 class='Review'>Reviews:</h1>");
-        foreach ($idarray as $id) {
-//                $id = getReviewID()
-                $sterren = getReviewAantSterren($databaseConnection,$_GET['id'],$id);
-                $naam = getReviewNaam($databaseConnection,$_GET['id'],$id);
-                $datum = getReviewDatum($databaseConnection,$_GET['id'],$id);
-                $omschrijving = getReviewOmschrijving($databaseConnection,$_GET['id'],$id);
-                $titel = getReviewOnderwerp($databaseConnection,$_GET['id'],$id);
-            for ($i = 0; $i < $sterren   ; $i++) {
-                print('<img src="Public/Img/starvol.png" style="height: 10%; width: 10%">');
-            }
-            for ($i = 0; $i < 5-$sterren   ; $i++) {
-                print('<img src="Public/Img/star.png" style="height: 10%; width: 10%">');
-            }
-            ?>
-            <br>
-            <h4><?php print("$naam");?> | <?php print("$datum");?> </h4>
-            <h3>(<?php print($omschrijving); ?>)</h3>
-        <?php }} ?>
-        </div>
-        <div>
         </div>
         <div id="StockItemSpecifications">
             <h3>Artikel specificaties</h3>
@@ -167,6 +138,38 @@ $idarray = getReviewIDUit($databaseConnection,$_GET['id']);
                 <?php
             }
             ?>
+        </div>
+            <?php
+            $idarray = getReviewIDUit($databaseConnection,$_GET['id']);
+
+            if (getReviewCount($databaseConnection,$_GET['id']) >= 1) {
+                ?><div id="StockItemReview"><?php
+
+                print("<h3 class='Review'>Reviews:</h3>");
+                ?><div style="margin: 30px"><?php
+                foreach ($idarray as $id) {
+//                $id = getReviewID()
+                    $sterren = getReviewAantSterren($databaseConnection,$_GET['id'],$id);
+                    $naam = getReviewNaam($databaseConnection,$_GET['id'],$id);
+                    $datum = getReviewDatum($databaseConnection,$_GET['id'],$id);
+                    $omschrijving = getReviewOmschrijving($databaseConnection,$_GET['id'],$id);
+                    $titel = getReviewOnderwerp($databaseConnection,$_GET['id'],$id);
+                    for ($i = 0; $i < $sterren   ; $i++) {
+                        print('<img src="Public/Img/starvol.png" style="height: 10%; width: 10%">');
+                    }
+                    for ($i = 0; $i < 5-$sterren   ; $i++) {
+                        print('<img src="Public/Img/star.png" style="height: 10%; width: 10%">');
+                    }
+                    ?>
+                        Allo
+                    <br>
+                    <h4><?php print("$naam");?> | <?php print("$datum");?> </h4>
+                    <h3>(<?php print($omschrijving); ?>)</h3>
+                <?php }} ?>
+        <form method="post">
+            <input type="submit" name="button2" value="Plaats een review voor dit product!" class="button" style="margin-top: 20px; margin-left: 16%">
+        </form>
+        </div>
         </div>
         <?php
     } else {
