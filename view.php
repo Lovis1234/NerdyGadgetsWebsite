8047ca06f51457cb8cdf2fadf60c5d306001f0af
Lovis1234 <90246498+Lovis1234@users.noreply.github.com>
2021-12-10 08:57:34 +0100

    jemoeder

--- view.php
+++ view.php
@@ -24,13 +24,19 @@ $button="";
         <div id="ArticleHeader">
             <?php
             if (isset($StockItemImage)) {
+                if (count($StockItemImage) == 0) {
+                    ?>
+                    <div id="ImageFrame"
+                         style="background-image: url('Public/StockItemIMG/GeenAfbeelding.jpg'); background-size: 250px; background-repeat: no-repeat; background-position: center;"></div>
+                    <?php
+                }
                 // één plaatje laten zien
-                if (count($StockItemImage) == 1) {
+                elseif (count($StockItemImage) == 1) {
                     ?>
                     <div id="ImageFrame"
                          style="background-image: url('Public/StockItemIMG/<?php print $StockItemImage[0]['ImagePath']; ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;"></div>
                     <?php
-                } else if (count($StockItemImage) >= 2) { ?>
+                } elseif (count($StockItemImage) >= 2) { ?>
                     <!-- meerdere plaatjes laten zien -->
                     <div id="ImageFrame">
                         <div id="ImageCarousel" class="carousel slide" data-interval="false">
@@ -174,16 +180,9 @@ if(isset($_POST["button"])) {
     addProductToCart("$id",$databaseConnection);
 }
 if(isset($_GET["button-minder"])) {
-    if( $cart[$_GET["idprod"]] != 1){
-        $cart[$_GET["idprod"]] -= 1;
-        saveCart($cart,$databaseConnection);
-        header("Location:Cart.php");
+            header('Location:reviewmaken.php?id='.$_GET["id"]);
     }
-    else
-    {
-        header('Location:verwijder.php?idprod='.$_GET["id"]);
-    }
-}
+
 ?>
 <?php
 include __DIR__ . "/footer.php";
