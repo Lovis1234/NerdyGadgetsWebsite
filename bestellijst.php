3e075c5cac1651b515cdcd9a40c6c3825dc48371
JonathanEverts <94914995+JonathanEverts@users.noreply.github.com>
2021-12-20 14:51:47 +0100

    Update

--- bestellijst.php
+++ bestellijst.php
@@ -109,50 +109,77 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     $factuurpostcode = getProfiel($databaseConnection, $email,"factuurpostcode");
     $factuurplaats = getProfiel($databaseConnection, $email,"factuurplaats");
     if(empty($voornaam) OR empty($achternaam) OR empty($factuurnaam) OR empty($factuurhuisnummer) OR empty($factuurstraat) OR empty($factuurpostcode) OR empty($factuurplaats) OR empty($bestelnaam) OR empty($bestelhuisnummer) OR empty($bestelstraat) OR empty($bestelpostcode) OR empty($bestelplaats)  ){
-        echo '<div class="row">
-    <div class="col-75">
-    <div class="container">
-    Before u can go further fill in, all your info in profile!
-    <a href="profile.php"><input type="submit" name="profile" value="Go now!"></a>
-    </div></div></div>';
+        echo '<div style="text-align: center;"><h1>Finish order</h1></div>
+<div class="bestellijst">
+    <table style="border= 1px solid red">
+    <tr>
+        <table style="text-align: center; width: 100%">
+            <tr>
+                <td colspan="1">
+                    Before u can go further fill in, all your info in profile!
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="profile.php"><input type="submit" name="profile" value="Go now!"></a>
+                </td>
+            </tr>
+        </table>
+    </div>';
     }
     else{
     ?>
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <body>
-    <center><h1>Afronden bestelling</h1></center>
-    <div class="row">
-    <div class="col-75">
-    <div class="container">
-    <form action="bestellijst.php">
-        <div class="row">
-            <div class="col-50">
-                <h3>Contactgegevens</h3><center>
-                Adres:<br>
-                  <input type="radio" id="man" name="geslacht" value="Order">
-                  <label for="man">Orderadress</label>
-                  <input type="radio" id="vrouw" name="geslacht" value="Invoice">
-                      <label for="css">Invoiceadress</label><br></center>
-                Verzendmethode: <SELECT name="vz" required>
-                    <option value="">Selecteer een verzendmethode</option>
-                    <?php
-                    $resultaat = getVmet($databaseConnection);
-                    foreach ($resultaat as $resultaten) {
-                        echo '<option value="'.$resultaten["DeliveryMethodID"].'">'.$resultaten["DeliveryMethodName"].'</option>';
-                    }?>
-                </SELECT> <br><br>
-                Betaalmethode: <SELECT name="bm" required>
-                    <option value="">Selecteer een betaalmethode</option>
-                    <?php
-                    $resultaat = getBetaal($databaseConnection);
-                    foreach ($resultaat as $resultaten) {
-                        echo '<option value="'.$resultaten["PaymentMethodID"].'">'.$resultaten["PaymentMethodName"].'</option>';
-                    }?>
-                </SELECT> <br><br>
-                <input type="submit" name="bestel2" value="Submit">
-    </form>
-
+        <div style="text-align: center;"><h1>Finish order</h1></div>
+        <div class="bestellijst">
+        <h3>Contact information</h3>
+        <form action="bestellijst.php">
+        <table style="border= 1px solid red">
+            <tr>
+                <table style="text-align: center; width: 100%">
+                    <tr>
+                        <td>
+                            <SELECT name="geslacht" required>
+                                <option value="">Select your adress</option>
+                                <option value="Order">Orderadress</option>
+                                <option value="Invoice">Invoiceadress</option>
+                            </SELECT>
+                        </td>
+                    </tr>
+                    <tr>
+                        <table style="text-align: left; width: 100%">
+                            <tr>
+                                <td>
+                                    Shipping method: <SELECT name="vz" required>
+                                        <option value="">Select shipping method</option>
+                                        <?php
+                                        $resultaat = getVmet($databaseConnection);
+                                        foreach ($resultaat as $resultaten) {
+                                            echo '<option value="'.$resultaten["DeliveryMethodID"].'">'.$resultaten["DeliveryMethodName"].'</option>';
+                                        }?>
+                                    </SELECT>
+                                </td>
+                                <td>
+                                    Paying method: <SELECT name="bm" required>
+                                        <option value="">Select paying method</option>
+                                        <?php
+                                        $resultaat = getBetaal($databaseConnection);
+                                        foreach ($resultaat as $resultaten) {
+                                            echo '<option value="'.$resultaten["PaymentMethodID"].'">'.$resultaten["PaymentMethodName"].'</option>';
+                                        }?>
+                                    </SELECT>
+                                </td>
+                            </tr>
+                        </table>
+                    </tr>
+                </table>
+            </tr>
+        </table>
+        <br>
+        <input type="submit" name="bestel2" value="Submit" style="width: 100%">
+        </form>
     <?php
 
 }}
@@ -169,59 +196,99 @@ else{
 
 </head>
 <body>
-<center><h1>Checkout</h1></center>
-    <div class="row">
-        <div class="col-75">
-             <div class="container">
-                <form action="bestellijst.php">
-                    <div class="row">
-                        <div class="col-50">
-                            <h3>Contact information</h3>
-    Email: <input type="text" id="email" name="email" required><br>
-    Gender:
-      <input type="radio" id="man" name="geslacht" value="man">
-      <label for="man">Male</label>
-      <input type="radio" id="vrouw" name="geslacht" value="vrouw">
-      <label for="css">Female</label>
-      <input type="radio" id="anders" name="geslacht" value="anders">
-      <label for="anders">Other</label><br>
-    First name: <input type="text" id="email" name="vn" required>
-                            Surname: <input type="text" id="email" name="an" required>
-    <SELECT name="beoordeling" required>
-        <option value="">Select a country</option>
-      <?php
-      $resultaat = getCountries($databaseConnection);
-      foreach ($resultaat as $resultaten) {
-          echo '<option value="'.$resultaten["CountryID"].'">'.$resultaten["CountryName"].'</option>';
-      }?>
+    <div style="text-align: center;"><h1>Finish order</h1></div>
+<div class="bestellijst">
+    <h3>Contact information</h3>
 
-       </SELECT> <br><br>
-    Zip/Postal code: <input type="text" id="zip" name="zip" required><br>
-    House number: <input type="text" id="huisnummer" name="huisnummer" required>
-                            Toevoeging: <input type="text" id="toevoeging" name="toevoeging"><br>
-    Street: <input type="text" id="straat" name="straat" required><br>
-    City: <input type="text" id="plaats" name="plaats" required><br>
-    Phone: <input type="text" id="telnummer" name="telnummer" required><br>
-                            Shipping method: <SELECT name="vz" required>
-                                <option value="">Select a shipping method</option>
-                                <?php
-                                $resultaat = getVmet($databaseConnection);
-                                foreach ($resultaat as $resultaten) {
-                                    echo '<option value="'.$resultaten["DeliveryMethodID"].'">'.$resultaten["DeliveryMethodName"].'</option>';
-                                }?>
-                            </SELECT> <br><br>
-                            Payment: <SELECT name="bm" required>
-                                <option value="">Select a payment method</option>
-                                <?php
-                                $resultaat = getBetaal($databaseConnection);
-                                foreach ($resultaat as $resultaten) {
-                                    echo '<option value="'.$resultaten["PaymentMethodID"].'">'.$resultaten["PaymentMethodName"].'</option>';
-                                }?>
-                            </SELECT> <br><br>
-    <input type="submit" name="bestel" value="Submit">
-</form>
-
-
+    <table style="border= 1px solid red">
+        <tr>
+            <table style="text-align: center; width: 100%">
+                <tr>
+                    <td colspan="1">
+                        First name: <input type="text" id="email" name="vn" placeholder="First name" required>
+                    </td>
+                    <td>
+                        Last name: <input type="text" id="email" name="an" placeholder="Last name" required>
+                    </td>
+                    <td style="width: 14%">
+                        <SELECT name="geslacht" required>
+                            <option value="">Select your gender</option>
+                            <option value="man">Man</option>
+                            <option value="vrouw">Woman</option>
+                            <option value="anders">Different</option>
+                        </SELECT>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <SELECT name="beoordeling" required>
+                            <option value="">Select your country</option>
+                            <?php
+                            $countries = getCountries($databaseConnection);
+                            foreach ($countries as $aap) {
+                                echo '<option value="'.$aap["CountryID"].'">'.$aap["CountryName"].'</option>';
+                            }?>
+                        </SELECT>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Street: <input type="text" id="straat" name="straat" placeholder="Street" required>
+                    </td>
+                    <td>
+                        House number: <input type="text" id="huisnummer" name="huisnummer" placeholder="House number" required>
+                    </td>
+                    <td style="width: 14%">
+                        Addition: <input type="text" id="toevoeging" name="toevoeging" placeholder="Addition">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="width: 30%">
+                        Zipcode: <input type="text" id="zip" name="zip" placeholder="Zipcode" required>
+                    </td>
+                    <td>
+                        City: <input type="text" id="plaats" name="plaats" placeholder="City" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        Email: <input type="text" id="email" name="email" placeholder="Email" required>
+                    </td>
+                    <td>
+                        Telephone number: <input type="text" id="telnummer" name="telnummer" placeholder="Telephone number" required>
+                    </td>
+                </tr>
+                <tr>
+                    <table style="text-align: left; width: 100%">
+                        <tr>
+                            <td>
+                                Shipping method: <SELECT name="vz" required>
+                                    <option value="">Select shipping method</option>
+                                    <?php
+                                    $countries = getVerzend($databaseConnection);
+                                    foreach ($countries as $aap) {
+                                        echo '<option value="'.$aap["DeliveryMethodID"].'">'.$aap["DeliveryMethodName"].'</option>';
+                                    }?>
+                                </SELECT>
+                            </td>
+                            <td>
+                                Paying method: <SELECT name="bm" required>
+                                    <option value="">Select paying method</option>
+                                    <?php
+                                    $countries = getBetaal($databaseConnection);
+                                    foreach ($countries as $aap) {
+                                        echo '<option value="'.$aap["PaymentMethodID"].'">'.$aap["PaymentMethodName"].'</option>';
+                                    }?>
+                                </SELECT>
+                            </td>
+                        </tr>
+                    </table>
+                </tr>
+            </table>
+        </tr>
+    </table>
+    <br>
+    <input type="submit" name="bestel" value="Submit" style="width: 100%">
 <?php
 }
 include __DIR__ . "/footer.php";
