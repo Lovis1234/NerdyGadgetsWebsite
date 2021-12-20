0e2d2b5227664bda1291359f8164e514e72f7ce4
JonathanEverts <94914995+JonathanEverts@users.noreply.github.com>
2021-12-20 16:11:28 +0100

    J

--- database.php
+++ database.php
@@ -59,7 +59,7 @@ function getStockItem($id, $databaseConnection) {
            SELECT SI.StockItemID, 
             (RecommendedRetailPrice*(1+(TaxRate/100))) AS SellPrice, 
             StockItemName, TaxRate,
-            CONCAT('Voorraad: ',QuantityOnHand)AS QuantityOnHand,
+            CONCAT('Stock: ',QuantityOnHand)AS QuantityOnHand,
             SearchDetails,IsChillerStock, 
             (CASE WHEN (RecommendedRetailPrice*(1+(TaxRate/100))) > 50 THEN 0 ELSE 6.95 END) AS SendCosts, MarketingComments, CustomFields, SI.Video,
             (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath   
