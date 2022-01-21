58f43595404b5f74c9a92e548e09ef6842184bb7
Tamimm22 <95698319+Tamimm22@users.noreply.github.com>
2022-01-21 03:00:31 +0100

    Update verwijder.php

--- verwijder.php
+++ verwijder.php
@@ -1,11 +1,10 @@
 <?php
-include "Functies.php";
+include "Functies.php"; //pagina include
 include __DIR__ . "/header.php";
-//$cart = array(1 => 1);
-$cart = getCart($databaseConnection);
-$id = $_GET['idprod'];
-$StockItem = getStockItem($id, $databaseConnection);
-//print_r($cart); ?>
+$cart = getCart($databaseConnection); //Hiermee krijg je de huidige carsessie in de array
+$id = $_GET['idprod']; //Pakt de id van het product die verwijderd word van de winkelwagen, dat doet die met een GET
+$StockItem = getStockItem($id, $databaseConnection); //Met het id van hier boven pakken wij alle gegevens van het product met de standaard fucntie get stock item. (deze functie zat al ik nerdygadgets)
+ ?>
 <center>
 <h1>Are you sure you want to remove <?php print $StockItem['StockItemName']; ?> from your cart?</h1>
 <form>
@@ -15,12 +14,11 @@ $StockItem = getStockItem($id, $databaseConnection);
 </form>
 </center>
 <?php
-if(isset($_GET["doorgaan"])) {//controleren of $stockItemID(=key!) al in array staat
-    unset($cart[$id]);                   //zo ja:  aantal met 1 verhogen
-    saveCart($cart,$databaseConnection);                            // werk de "gedeelde" $_SESSION["cart"] bij met de bijgewerkte cart
-    print_r($cart);
-    header("Location:Cart.php");
+if(isset($_GET["doorgaan"])) {//Als je op de knop doorgaan klikt dan doet die deze if.
+    unset($cart[$id]);                   //Met de functie unset verwijderen wij iets van een array. Wat wij hier verwijderen is de id van het product die wij willen verwijderen
+    saveCart($cart,$databaseConnection);  //Hiermee slaan wij de verandering op.
+    header("Location:Cart.php");  //Met de header kunnen wij weer terug gaan naar de winkelmand pagina.
 }
-if(isset($_GET["annuleren"])) {//controleren of $stockItemID(=key!) al in array staat
-    header("Location:Cart.php");
+if(isset($_GET["annuleren"])) { //Als je op de knop annuleren klikt dan voert die dit onder uit.
+    header("Location:Cart.php"); //Met de header kunnen wij weer terug gaan naar de winkelmand pagina.
 }
