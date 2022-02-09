1fbdc512e000f88820378fb5192b81cf83adb5cd
Tamimm22 <95698319+Tamimm22@users.noreply.github.com>
2022-02-09 23:18:25 +0100

    Update verwijder.php

--- verwijder.php
+++ verwijder.php
@@ -1,9 +1,9 @@
 <?php
 include "Functies.php"; //pagina include
 include __DIR__ . "/header.php";
-$cart = getCart($databaseConnection); //Hiermee krijg je de huidige carsessie in de array
-$id = $_GET['idprod']; //Pakt de id van het product die verwijderd word van de winkelwagen, dat doet die met een GET
-$StockItem = getStockItem($id, $databaseConnection); //Met het id van hier boven pakken wij alle gegevens van het product met de standaard fucntie get stock item. (deze functie zat al ik nerdygadgets)
+$cart = getCart($databaseConnection);
+$id = $_GET['idprod'];
+$StockItem = getStockItem($id, $databaseConnection);
  ?>
 <center>
 <h1>Are you sure you want to remove <?php print $StockItem['StockItemName']; ?> from your cart?</h1>
@@ -14,11 +14,11 @@ $StockItem = getStockItem($id, $databaseConnection); //Met het id van hier boven
 </form>
 </center>
 <?php
-if(isset($_GET["doorgaan"])) {//Als je op de knop doorgaan klikt dan doet die deze if.
-    unset($cart[$id]);                   //Met de functie unset verwijderen wij iets van een array. Wat wij hier verwijderen is de id van het product die wij willen verwijderen
-    saveCart($cart,$databaseConnection);  //Hiermee slaan wij de verandering op.
-    header("Location:Cart.php");  //Met de header kunnen wij weer terug gaan naar de winkelmand pagina.
+if(isset($_GET["doorgaan"])) {
+    unset($cart[$id]);
+    saveCart($cart,$databaseConnection);
+    header("Location:Cart.php");
 }
-if(isset($_GET["annuleren"])) { //Als je op de knop annuleren klikt dan voert die dit onder uit.
-    header("Location:Cart.php"); //Met de header kunnen wij weer terug gaan naar de winkelmand pagina.
+if(isset($_GET["annuleren"])) {
+    header("Location:Cart.php");
 }
