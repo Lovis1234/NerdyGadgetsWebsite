5e709a8941e6f27fb7d6b0de9d0ee7729f26a2ef
Tamimm22 <95698319+Tamimm22@users.noreply.github.com>
2022-02-11 14:57:54 +0100

    date

--- header.php
+++ header.php
@@ -78,7 +78,7 @@ $databaseConnection = connectToDatabase();
             <div id="SubContent">
                 <br>
                 <?php
-                if (date("h") > 10 AND date("h") < 12   ){
+                if (date("h") > 0 AND date("h") < 2   ){
                     print "<center>Je happyhour kortingscode: 'happyhour' (20%)</center>";
                     $sql_coupon = "SELECT couponcode FROM coupons WHERE couponcode = 'happyhour'";
                     $declaratie = mysqli_query($databaseConnection, $sql_coupon);
